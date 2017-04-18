7a90de1776346f3db2a68caefb001c65cc4162d4
Giang Trinh <Giang@key-simple.com>
2017-04-19 00:35:17 +0700

    new branch

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379"); 
+            ISubscriber sub = redis.GetSubscriber();
+            //IDatabase db = redis.GetDatabase();
+            //string value = db.StringGet("recent_popular_searchs");
+            //Console.WriteLine(value); // writes: "abcdefg"
+            //Console.WriteLine("data: ");
+            sub.Publish("messages", "hello"); 
+            //sub.Subscribe("messages", (channel, message) =>
+            //{
+            //    Console.WriteLine((string)message);
+            //});
+           
+            Console.ReadLine();
+        }
+    }
+}
